2a36351e092f5ba8b34f8413e68e8015583917c3
Anastasiia <nastena.stuzhuk@gmail.com>
2023-06-20 16:10:00 +0300

    add id property to card object

--- src/data.js
+++ src/data.js
@@ -1,14 +1,16 @@
 export default [
   {
-      title: "Mount Fuji",
-      location: "Japan",
-      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-      startDate: "12 Jan, 2021",
-      endDate: "24 Jan, 2021",
-      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    id: 1,
+    title: "Mount Fuji",
+    location: "Japan",
+    googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+    imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
   },
   {
+    id: 2,
     title: "Sydney Opera House",
     location: "Australia",
     googleMapsUrl: "https://goo.gl/maps/FVP68m1fcfLVEGc59",
@@ -18,6 +20,7 @@ export default [
     imageUrl: "https://source.unsplash.com/JmuyB_LibRo"
   },
   {
+    id: 3,
     title: "Geirangerfjord",
     location: "Norway",
     googleMapsUrl: "https://goo.gl/maps/Y6MC9a6viCnTyEuu7",
